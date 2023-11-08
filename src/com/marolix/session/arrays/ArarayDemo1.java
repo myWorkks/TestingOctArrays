3938ed1f224cea5c4d965e1349b5adb64e2a144d
nsgbharath <reddysbharath45@gmail.com>
2023-11-08 11:43:02 +0530

    array declaration and initilisation

---
+++ src/com/marolix/session/arrays/ArarayDemo1.java
@@ -0,0 +1,36 @@
+package com.marolix.session.arrays;
+
+public class ArarayDemo1 {
+
+	public static void main(String[] args) {
+		int a1 = 10;
+		int a2 = 20;
+		int a3 = -1;
+		int a4 = 8;
+		int[] a;
+//		int a22[];
+//		int []a23;
+//		int a24 [];
+		//byte b = 1000;
+		String s;
+		s = new String();
+		String s1 = new String();
+		a = new int[8];
+		int ad[] = new int[10];
+		// data_type [] variable=new data_type[int size];
+		
+		String name = "bhrath";
+		char ch='a';
+		float f=10.0f;
+		long l=21212121212121L;
+		int ad1[] = new int[10];		
+		int ad2[]= new int[20];
+		int ad3[]= new int[0];
+	//	int ad4[]=new int [-20];//negativeindexexceptin
+		float f1=10.002020202547848484848f;
+		double d1=10.002020202547848484848;
+		System.out.println(f1);
+		System.out.println(d1);
+	}
+
+}
---
+++ src/com/marolix/session/arrays/ArrayDemo2.java
@@ -0,0 +1,20 @@
+package com.marolix.session.arrays;
+
+public class ArrayDemo2 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[10];//last index= size-1
+		
+		System.out.println("value at " + 0 + " index " + arr[0]);
+		System.out.println("value at " + 1 + " index " + arr[1]);
+		System.out.println("value at " + 2 + " index " + arr[2]);
+		System.out.println("value at " + 3 + " index " + arr[3]);
+		System.out.println("value at " + 4 + " index " + arr[4]);
+		System.out.println("value at " + 5 + " index " + arr[5]);
+		System.out.println("value at " + 6 + " index " + arr[6]);
+		System.out.println("value at " + 7 + " index " + arr[7]);
+		System.out.println("value at " + 8 + " index " + arr[8]);
+		System.out.println("value at " + 9 + " index " + arr[9]);
+	}
+
+}
